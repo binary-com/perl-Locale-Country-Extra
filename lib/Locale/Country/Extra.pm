dea51104a3cc5e07da236b4d532f39acbe57ee34
Amin Marashi <marashi.s.amin@gmail.com>
2018-11-26 18:37:24 +0800

    Remove extra line

--- lib/Locale/Country/Extra.pm
+++ lib/Locale/Country/Extra.pm
@@ -80,6 +80,7 @@ sub all_country_codes {
 sub localized_code2country {
     my ( $self, $country_code, $lang ) = @_;
 
+
     my $country_str = $lcm->code2country( $country_code, $lang );
 
     # Replace the initial Unicode A in Aland Islands with an ASCII A for usability purposes
